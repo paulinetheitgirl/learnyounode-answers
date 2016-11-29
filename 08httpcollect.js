798025bda65317eb8bbc54e9f0f557f74169401e
paulinetheitgirl <pdpdizon@gmail.com>
2016-11-30 00:35:23 +1100

    Exercises 8 and 9

---
+++ 08httpcollect.js
@@ -0,0 +1,20 @@
+var http = require('http');
+
+var url = process.argv[2];
+http.get(url,
+	function(response){
+		response.setEncoding("utf8");
+		var allData = '';
+
+		response.on("data", function(data){
+			allData += data;
+		});
+		response.on("error", function(){
+			console.log("There was an error");
+		});
+		response.on("end", function(){
+			console.log(allData.length);
+			console.log(allData);
+		});
+	});
+
---
+++ 09juggleasync.js
@@ -0,0 +1,28 @@
+// Assume that 3 URLs are provided as arguments
+var http = require('http');
+
+var urls = process.argv.slice(2);
+var ended = 0;
+var responses = ['', '', ''];
+
+urls.forEach(function(element, index){
+	http.get(element,
+		function(response){
+			response.setEncoding("utf8");
+
+			response.on("data", function(data){
+				responses[index] += data;
+			});
+			response.on("error", function(){
+				console.log("There was an error");
+			});
+			response.on("end", function(){
+				ended++;
+				if(ended == 3) {
+					responses.forEach(function(responseString){
+						console.log(responseString);
+					});
+				}
+			});
+		});
+});
